fd4c9365cb2e0240704b1f28c26c318ad27ecf46
dianka-dal <dianka.dal@gmail.com>
2021-08-06 07:46:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import configparser
-import json
 import csv
-
 from telethon.sync import TelegramClient
-from telethon.errors import SessionPasswordNeededError
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
 
 api_id = 0000000
 api_hash = ''
@@ -21,14 +15,14 @@ if not client.is_user_authorized():
 all_participants = []
 all_participants = client.get_participants('')
 
-print('Saving In file...')
 with open("mm_members_list.csv", "w", encoding='UTF-8') as f:
     writer = csv.writer(f, delimiter=",", lineterminator="\n")
-    writer.writerow(['User name', 'User Id'])
+    writer.writerow(['User Alias', 'User ID'])
     for user in all_participants:
         if user.username:
             username = user.username
         else:
             continue
         writer.writerow([username, user.id])
+        
 print('Data scraped successfully.')
